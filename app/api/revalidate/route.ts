bdee46b3792dd71002c79c98bc4f386ae1f84eb2
Frederik Fink Højriis <58032545+frederikfink@users.noreply.github.com>
2024-08-12 20:31:04 +0200

    Update route.ts

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,9 @@
 export async function POST(req: Request) {
   try {
-    const body = (await req.json()) as { slug: string };
-    console.log(body);
+    const body = (await req.json()) as any;
+    console.log(body?.related_entities);
+
+    return new Response("OK", { status: 200 });
   } catch (error) {
     return new Response("Internal Server Error", { status: 500 });
   }
