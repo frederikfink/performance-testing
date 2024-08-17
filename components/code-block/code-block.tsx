f2f4316fb45b49648e6717bab2f0f6fa52ad623e
Frederik Fink Højriis <58032545+frederikfink@users.noreply.github.com>
2024-08-17 22:26:05 +0200

    add copy button functionality

--- components/code-block/code-block.tsx
+++ components/code-block/code-block.tsx
@@ -6,12 +6,22 @@ import {
   transformerNotationHighlight,
 } from "@shikijs/transformers";
 import CopyCodeButton from "./copy-code-button";
+import { FileJs, FileTs } from "@phosphor-icons/react/dist/ssr";
 
 interface Props {
   code: string;
   language: string;
   title?: string;
 }
+
+const getIcon = (language: string) => {
+  switch (language) {
+    case "javascript":
+      return <FileJs size={16} />;
+    case "typescript":
+      return <FileTs size={16} />;
+  }
+};
 
 const CodeBlock = async ({ code, title, language }: Props) => {
   const highlighter = await createHighlighter({
@@ -32,12 +42,13 @@ const CodeBlock = async ({ code, title, language }: Props) => {
 
   return (
     <div className="rounded-lg border">
-      <div className="px-4 py-3 text-sm bg-muted dark:bg-black items-center justify-between rounded-t-lg text-muted-foreground flex">
-        <p>{title}</p>
-        <CopyCodeButton code={code} />
+      <div className="px-4 py-3 text-sm bg-muted gap-2 dark:bg-black items-center rounded-t-lg text-muted-foreground flex">
+        {getIcon(language)}
+        <p className="text-[13px]">{title}</p>
+        <CopyCodeButton className="ml-auto" code={code} />
       </div>
       <div
-        className="[&>pre]:rounded-b-lg [&>pre]:py-4 [&>pre]:px-2 text-[13px] border-t"
+        className="[&>pre]:rounded-b-lg [&>pre]:py-4 [&>pre]:px-2 text-[13px] border-t overflow-x-auto"
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </div>
--- components/code-block/copy-code-button.tsx
+++ components/code-block/copy-code-button.tsx
@@ -1,18 +1,39 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { Check, Clipboard } from "@phosphor-icons/react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   code: string;
+  className?: string;
 }
 
-const CopyCodeButton = ({ code }: Props) => {
-  const copyCode = () => {
+const CopyCodeButton = ({ code, className }: Props) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyCode = async () => {
     navigator.clipboard.writeText(code);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
   };
 
   return (
-    <Button size={"sm"} variant={"outline"} onClick={copyCode}>
+    <Button
+      size={"sm"}
+      variant={"outline"}
+      onClick={copyCode}
+      className={cn("gap-2", className)}
+    >
+      {isCopied ? (
+        <Check weight="bold" className="rotate-in" size={14} />
+      ) : (
+        <Clipboard weight="bold" className="rotate-in" size={14} />
+      )}
       Copy
     </Button>
   );
